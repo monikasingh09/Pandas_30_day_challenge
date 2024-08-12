07181001a27665c6112eace275860af65ed41c67
monikasingh09 <35740692+monikasingh09@users.noreply.github.com>
2024-08-12 18:50:40 +0200

    Update pandas.py

--- Data_Filtering/pandas.py
+++ Data_Filtering/pandas.py
@@ -10,3 +10,15 @@ def find_products(products: pd.DataFrame) -> pd.DataFrame:
     df = products[(products['low_fats'] == 'Y') & (products['recyclable'] == 'Y')]
     return df[['product_id']]
 
+
+def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+    df = customers[~customers['id'].isin(orders['customerId'])]
+    return df[['name']].rename(columns = {'name':'Customers'})
+
+
+def article_views(views: pd.DataFrame) -> pd.DataFrame:
+    df = views[views['author_id'] == views['viewer_id']]
+    udf = df['author_id'].unique()
+    udf = sorted(udf)
+    return pd.DataFrame({'id':udf})
+
