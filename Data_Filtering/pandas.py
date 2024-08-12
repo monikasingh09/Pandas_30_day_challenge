d2f84ea5b22bfc4c0ab395b6facec6a9d6fca3c2
monikasingh09 <35740692+monikasingh09@users.noreply.github.com>
2024-08-12 18:18:49 +0200

    Update pandas.py

--- Data_Filtering/pandas.py
+++ Data_Filtering/pandas.py
@@ -5,3 +5,8 @@ def big_countries(world: pd.DataFrame) -> pd.DataFrame:
     return df[['name','population','area']]
 
 
+
+def find_products(products: pd.DataFrame) -> pd.DataFrame:
+    df = products[(products['low_fats'] == 'Y') & (products['recyclable'] == 'Y')]
+    return df[['product_id']]
+
